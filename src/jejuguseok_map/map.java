53bcb06f79600311dd62c8bf7cee23a68db79c82
KyeongHwanMin <h45652@naver.com>
2021-07-07 14:50:19 +0900

    [민경환] 정현서 index.jsp 오류 해결됨

--- src/jejuguseok_map/map.java
+++ src/jejuguseok_map/map.java
@@ -1,9 +1,11 @@
 package jejuguseok_map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-//http://localhost:8080/jejuguseok/index.do
+//http://localhost:8080/jejuguseok/form.do
 @Controller
 public class map {
 	
@@ -12,10 +14,36 @@ public class map {
 	
 	@RequestMapping("map.do")
 	public String map() {
-		System.out.println("mapdto--"+mapdto.getX());
-		System.out.println("mapdto--"+mapdto.getPlace());
+		//System.out.println("mapdto--"+mapdto.getX());
+		//System.out.println("mapdto--"+mapdto.getPlace());
 		
-		return "/WEB-INF/view/userpage/map.jsp";
+		return "/WEB-INF/view/map/map.jsp";
+	}
+	
+	@RequestMapping("mapPro.do")
+	public String map2(String name, int age, mapDTO mapdto, HttpServletRequest request) {
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(mapdto.getX());
+		System.out.println(request.getParameter("id"));
+		
+		String id = request.getParameter("id");
+		request.setAttribute("id", id);
+		return "/WEB-INF/view/map/map2.jsp";
+	}
+	
+	@RequestMapping("main.do")
+	public String main() {
+		System.out.println("main()");
+		return "/WEB-INF/view/map/form.jsp";
+	}
+	@RequestMapping("formPro.do")
+	public String pro(String name, int age, HttpServletRequest request) {
+		System.out.println(name);
+		System.out.println(age);
+		request.setAttribute("name", name);
+		request.setAttribute("age", age);
+		return "/WEB-INF/view/map/formPro.jsp";
 	}
 
 }
