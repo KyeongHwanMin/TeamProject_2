6298852050370a9258b72464bcbb773ccf19ac04
gnstj <gnstj@118.221.54.194>
2021-07-08 18:28:28 +0900

    박성훈 / oto list 오류 해결하면 옮길것

--- src/userpage/board/otoBean.java
+++ src/userpage/board/otoBean.java
@@ -1,6 +1,23 @@
 package userpage.board;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class otoBean {
 	
+	@Controller
+	public class BoardDAO {
+
+		@Autowired
+		private SqlSessionTemplate dao = null;
+		
+		@RequestMapping("/mybatis/list.do")
+		public String form() {
+			
+			return "/board/list";
+		}
+	}
 
 }
