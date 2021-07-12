8f6d0cfac334097b866089d8c2ad900f4936812a
flyty <flyty@DESKTOP-VCLH10G>
2021-07-12 19:36:27 +0900

    작성자:염민영 관광지 숙소 Form/Pro 등 작성

---
+++ src/jejuguseok_map/ItemDTO.java
@@ -0,0 +1,42 @@
+package jejuguseok_map;
+
+public class ItemDTO {
+
+	private String place_name;
+	private String address;
+	private String content;
+	private String category;
+	private String img;
+	public String getPlace_name() {
+		return place_name;
+	}
+	public void setPlace_name(String place_name) {
+		this.place_name = place_name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+	
+}
