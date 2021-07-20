c74dfefeee54cef21df5ff65b8b11d7377f31ce5
flyty <flyty@DESKTOP-VCLH10G>
2021-07-20 18:21:34 +0900

    작성: 염민영 내용 - 등록된 DB값 불러오기 sql문 추가 bean searchForm,Pro 추가 DAO 작업 중

--- src/jejuguseok_map/attractionDAOImpl.java
+++ src/jejuguseok_map/attractionDAOImpl.java
@@ -1,7 +1,11 @@
 package jejuguseok_map;
 
+import javax.servlet.http.HttpSession;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import userpage.main.userDTO;
 
 public class attractionDAOImpl implements attractionDAOInter {
 
@@ -14,7 +18,14 @@ public class attractionDAOImpl implements attractionDAOInter {
 		
 		attractionDAO.insert("item.insertAttraciton",dto);
 }
-//	찜한 관광지, 숙소 리스트(mypage에서 조회)
+	
+// 	관광지DB 불러오기 
+	@Override
+	public void getList(attractionDTO dto) throws Exception{
+		attractionDTO dto1 = attractionDAO.selectOne("item.getAttractionList",dto);
+	}
+	
+//	찜한 관광지 리스트(mypage에서 조회)
 	@Override
 	public void myAttraction(attractionDTO dto) throws Exception {
 	}
--- src/jejuguseok_map/attractionDAOInter.java
+++ src/jejuguseok_map/attractionDAOInter.java
@@ -3,5 +3,6 @@ package jejuguseok_map;
 public interface attractionDAOInter {
 
 	public void insert(attractionDTO dto) throws Exception; 
-	public void myAttraction(attractionDTO dto) throws Exception;  
+	public void myAttraction(attractionDTO dto) throws Exception;
+	public void getList(attractionDTO dto) throws Exception;  
 }
